bdc0b303281cee7dc389124c4648b654aba432a2
T-b-t-nchos <T-b-t-nchos@outlook.com>
2025-10-29 00:46:23 +0900

    ✨️ | Add "Preparing..." text on blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
 export default function BlogPage() {
-
+    return (
+        <div className="items-centerrelative min-h-screen flex items-center justify-center overflow-hidden bg-neutral-950">
+            <h1>Preparing...</h1>
+        </div>
+    );
 }
